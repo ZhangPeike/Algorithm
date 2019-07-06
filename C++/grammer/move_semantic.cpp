b802f38fd12ec426cacebeedaa889668be6b9c0b
Peike Zhang <zhangpeike@momenta.ai>
2019-07-06 21:35:22 +0800

    Add Effctive modern C++ C1

--- C++/grammer/move_semantic.cpp
+++ C++/grammer/move_semantic.cpp
@@ -1,22 +1,36 @@
+#include <algorithm>
 #include <array>
 #include <cstring>
+#include <eigen3/Eigen/Core>
 #include <iostream>
 // for typeid
 #include <typeinfo>
 #include <vector>
-// TODO： 程序退出后 new 占用内存是否会被清理
+// constexpr 编译期可用
 template <typename T, std::size_t N>
 constexpr std::size_t ArraySize(T (&)[N]) noexcept {
   return N;
 }
-
+void ProcessWidget(const bool IsHighPriority) {
+  if (IsHighPriority) {
+    std::cout << "VIP" << std::endl;
+  } else {
+    std::cout << "NOT VIP" << std::endl;
+  }
+}
+// 右值引用与转移语义
 class Widget {
 private:
   int length_;
   int *data_;
+  // 代理类: std::vector<bool>::reference
+  std::vector<bool> vec_bool_{true, false};
   //   static int cnt_ = 0;
 
 public:
+  // decltype(auto) VecBool() { return std::forward<std::vector<bool>>(vec_bool_[1]);
+  // };
+  decltype(auto) GetVecBool() { return vec_bool_; }
   Widget() {
     length_ = 0;
     data_ = nullptr;
@@ -63,6 +77,9 @@ public:
     std::cout << "Default dctor" << std::endl;
   }
 };
+// how should I use it.
+auto Less = [](const auto &p1, const auto &p2) { return *p1 < *p2; };
+auto Print = [](const auto &p) { std::cout << p << std::endl; };
 // Error
 // auto CreateList() { return {1, 2, 3}; }
 std::initializer_list<int> CreateList() { return {1, 2, 3}; }
@@ -78,7 +95,18 @@ decltype(auto) AuthenticateAndAccess(Container &c, Index i) {
   return std::forward<Container>(c[i]);
 }
 int main() {
+  // Eigen Matrix 是隐形代理类吗
+  Eigen::Matrix2d m1;
+  m1(0, 0) = 5;
+  m1(1, 0) = 5;
+  m1(0, 1) = 5;
+  m1(1, 1) = 5;
+  Eigen::Matrix2d m2 = Eigen::Matrix2d::Ones();
+  auto m = m1 + m2;
+  std::cout << "Type of eigen matrix sum: " << typeid(m).name() << std::endl;
   std::vector<int> d(6, 1);
+  // std::for_each(d.begin(), d.end(), [](auto &x) { std::cout << x << " "; });
+  std::for_each(d.begin(), d.end(), Print);
   AuthenticateAndAccessLvalue(d, 5) = 100;
   std::cout << "d[5]: " << d[5] << std::endl;
   std::vector<Widget> vec_Widget;
@@ -89,6 +117,13 @@ int main() {
   // Widget w1(9);
   // vec_Widget.push_back(Widget(9));
   Widget w = Widget(9);
+  // Error result
+  ProcessWidget(w.GetVecBool()[0]);
+  auto Priority = w.GetVecBool()[0];
+  ProcessWidget(Priority);
+  // Error result
+  auto PriorityNew = static_cast<bool>(w.GetVecBool()[0]);
+  ProcessWidget(PriorityNew);
   // Widget w2(10);
   // vec_Widget.push_back(std::move(w2));
   // Widget w3(11);
@@ -104,6 +139,5 @@ int main() {
   auto x2(29);
   auto x3{29};
   auto x4 = {29};
-
   return 0;
 }
