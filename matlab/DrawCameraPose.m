1097821ee107f8c07af061960ed15577814fc00a
zhangpeike <zhangpeike@momenta.ai>
2020-01-02 12:16:00 +0800

    make structure clear

--- matlab/DrawCameraPose.m
+++ matlab/DrawCameraPose.m
@@ -1,14 +1,9 @@
-% figure
-% plot3
 figure
 hold on
-grid on  
+grid on
 % xlim([-15,20]);
 % ylim([-15,20]);
 % zlim([15,25]);
-for row=1:775
-    R=rotationVectorToMatrix(pose(row, 2:4));
-    c=-R'*pose(row, 5:7)';
-    R=R';    
-    cam = plotCamera('Location',c,'Orientation',R,'Opacity',0.0, 'Size', 0.07, 'color', [0, 0, 1]);
-end
+c = -R' * t;
+R = R';
+cam = plotCamera('Location', c, 'Orientation', R, 'Opacity', 0.0, 'Size', 0.07, 'color', [0, 0, 1]);
